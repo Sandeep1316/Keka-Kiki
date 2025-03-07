92a80b0eb093291bdbb29ab04a6cbcb16713d2a1
Sandeep Pasupuleti <sandeep.pasupuleti@keka.com>
2025-03-07 12:17:18 +0530

    Add project files.

---
+++ IntentRecognition/IntentRecognitionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Azure.AI.TextAnalytics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KekaBot.kiki.IntentRecognition;
+
+public static class IntentRecognitionExtensions
+{
+    public static List<CategorizedEntity> EntityRecognition(this TextAnalyticsClient client, string text)
+    {
+        var response = client.RecognizeEntities(text);
+        return response.Value.Select(_ => _).ToList();
+    }
+
+    public static List<PiiEntity> RecognizePII(this TextAnalyticsClient client, string text)
+    {
+        PiiEntityCollection entities = client.RecognizePiiEntities(text).Value;
+        return entities.Select(_ => _).ToList();
+    }
+
+
+    public static List<LinkedEntity> RecognizeLinkedEntities(this TextAnalyticsClient client, string text)
+    {
+        var response = client.RecognizeLinkedEntities(text);
+        return response.Value.Select(_ => _).ToList();
+    }
+
+    public static List<string> ExtractKeyPhrases(this TextAnalyticsClient client, string text)
+    {
+        var response = client.ExtractKeyPhrases(text);
+        return response.Value.Select(_ => _).ToList();
+    }
+}
---
+++ IntentRecognition/IntentRecognizer.cs
@@ -0,0 +1,22 @@
+﻿using Azure;
+using System;
+using Azure.AI.TextAnalytics;
+using Microsoft.Extensions.Configuration;
+
+namespace KekaBot.kiki.IntentRecognition;
+
+public class IntentRecognizer
+{
+    public IntentRecognizer(IConfiguration configuration)
+    {
+        var endpoint = new Uri(configuration["CLU_Api_Endpoint"]);
+        var credentials = new AzureKeyCredential(configuration["CLU_Api_Key"]);
+        this.Client = new TextAnalyticsClient(endpoint, credentials);
+        // You will implement these methods later in the quickstart.
+    }
+
+    /// <summary>
+    /// Gets or sets the client.
+    /// </summary>
+    public TextAnalyticsClient Client { get; set; }
+}
