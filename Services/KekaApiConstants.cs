0239e92593ef2049355225e26af59ca0cb34a2c5
GouthamYasoju <goutham.y@keka.com>
2025-03-07 17:12:36 +0530

    Added Create Leave Endpoint

--- Services/KekaApiConstants.cs
+++ Services/KekaApiConstants.cs
@@ -4,7 +4,9 @@ namespace KekaBot.kiki.Services;
 
 public class KekaApiConstants
 {
-    public const string GetAllTicketCategories = "api/helpdesk/settings/categories";
+    public const string GetAllTicketCategories = "/api/helpdesk/settings/categories";
     public const string CreateTicket = "/api/me/helpdesk/addticket";
+
+    public const string RequestLeave = "/api/me/leave/request";
 }
     
--- Services/KekaServiceClient.cs
+++ Services/KekaServiceClient.cs
@@ -12,8 +12,9 @@ namespace KekaBot.kiki.Services
     public class KekaServiceClient
     {
         private static readonly HttpClient client = new HttpClient();
-        private readonly string baseUrl = "https://bestfriend.kekad.com/k/default";
-        private readonly string accessToken = "";
+        private readonly string helpdeskbaseUrl = "https://bestfriend.kekad.com/k/default";
+        private readonly string leavebaseurl = "https://bestfriend.kekad.com/k/leave";
+        private readonly string accessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBGQ0JCNEZEQzNERjQ0Nzk4RjQyREY3ODc3ODgwN0E1MUVFNzUzMkUiLCJ4NXQiOiJEOHUwX2NQZlJIbVBRdDk0ZDRnSHBSN25VeTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2xvZ2luLmtla2FkLmNvbSIsIm5iZiI6MTc0MTMzMDY2MSwiaWF0IjoxNzQxMzMwNjYxLCJleHAiOjE3NDE0MTcwNjEsImF1ZCI6WyJrZWthaHIuYXBpIiwiaGlyby5hcGkiLCJodHRwczovL2xvZ2luLmtla2FkLmNvbS9yZXNvdXJjZXMiXSwic2NvcGUiOlsib3BlbmlkIiwia2VrYWhyLmFwaSIsImhpcm8uYXBpIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbInB3ZCJdLCJjbGllbnRfaWQiOiIyZmNiZTdlMC0wZmI0LTRmNmQtODZiYy0xOWZmYzQyZjJmMjUiLCJzdWIiOiJjMTQxOTZhZC1jOTBiLTQzZGYtYWNkMC02YzI3MjIxMDJhNTYiLCJhdXRoX3RpbWUiOjE3NDEwODA2NDEsImlkcCI6ImxvY2FsIiwidGVuYW50X2lkIjoiZTNiYjEwZmMtYzM5NS00MTk2LTkxYTMtZGNiYTYwZjQzMzA4IiwidGVuYW50aWQiOiJlM2JiMTBmYy1jMzk1LTQxOTYtOTFhMy1kY2JhNjBmNDMzMDgiLCJzdWJkb21haW4iOiJiZXN0ZnJpZW5kLmtla2FkLmNvbSIsInVzZXJfaWQiOiJhZmQxNjJmNy02NzU1LTRiMTgtOThiOS00MTlhZmFjM2I0N2UiLCJ1c2VyX2lkZW50aWZpZXIiOiJhZmQxNjJmNy02NzU1LTRiMTgtOThiOS00MTlhZmFjM2I0N2UiLCJ1c2VybmFtZSI6ImdlbWluaS5tdXBwYXJ0aHlAZ21haWwuY29tIiwiZW1haWwiOiJnZW1pbmkubXVwcGFydGh5QGdtYWlsLmNvbSIsImF1dGhlbnRpY2F0aW9uX3R5cGUiOiIxIiwic2lkIjoiOEMwOEU2NkJFRTI3MTA3RURGM0MzNUQ2RDJGMDhFMzEiLCJqdGkiOiIwMzAwNEMzN0U4RDg5MDQ3ODI5QTdBQkEzMzc2QTQ5NyJ9.dlmThYXcfkSm5NXTmT9CMFzfi6uNRwMRKIDFyLalkSUneMenuXhyrTK-OBFxf9YsMzRNZSCZJSubRzmv29mDekylYJY2kIg2g1OB-A1RRkRFZPsh371MlZaohbf-m3xvu0DPBt1HmIcz4PNrSx3DGH7BT76ka0w3AIioZ2dcx0pgZkCJrewNym6XB-_lyXpI4nN85ZwEK25vARBikdvAcnWikSxEC-16l_G9eDPAeal1MvmzkV7vrVa0rKSWs8dleJLuhRAqpUqyU_XsAQujOtaDTnGIKk2tuPPutJ7niC-ICVazmXI-esTFTUIF1hr4dbFR-SK7lnqGuPynjgDMOA";
 
         public KekaServiceClient()
         {
@@ -21,13 +22,19 @@ namespace KekaBot.kiki.Services
 
         public async Task<TicketCategoryListItem> GetAllTicketCategoriesAsync()
         {
-            var requestUrl = BuildRestRequest(KekaApiConstants.GetAllTicketCategories);
+            var requestUrl = BuildRestRequest(this.helpdeskbaseUrl, KekaApiConstants.GetAllTicketCategories);
             return await ExecuteGetAsync<TicketCategoryListItem>(requestUrl, this.accessToken);
         }
 
-        public async Task PostTickect(RaiseTicketModel ticket)
+        public async Task PostTickect(LeaveRequest leaveRequest)
         {
-            var requestUrl = BuildRestRequest(KekaApiConstants.CreateTicket);
+            var requestUrl = BuildRestRequest(this.leavebaseurl, KekaApiConstants.RequestLeave);
+            await ExecutePostAsync<object>(requestUrl, leaveRequest, this.accessToken);
+        }
+        
+        public async Task RequestLeave(RaiseTicketModel ticket)
+        {
+            var requestUrl = BuildRestRequest(this.leavebaseurl, KekaApiConstants.CreateTicket);
             await ExecutePostAsync<object>(requestUrl, ticket, this.accessToken);
         }
 
@@ -72,9 +79,9 @@ namespace KekaBot.kiki.Services
         /// <summary>
         /// Builds the complete API request URL.
         /// </summary>
-        private string BuildRestRequest(string urlPath)
+        private string BuildRestRequest(string baseurl, string urlPath)
         {
-            return $"{this.baseUrl}{urlPath}";
+            return $"{baseurl}{urlPath}";
         }
     }
 }
